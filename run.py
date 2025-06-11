aa1c362da0c0e582380a52bc1e0b69f10e71c16e
pickles4evaaaa <king.gabriel@riseup.net>
2025-06-11 09:17:35 -0500

    Bind host to all itnerfaces for external access

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5054, host='127.0.0.1')
+    app.run(debug=False, port=5054, host='0.0.0.0')
