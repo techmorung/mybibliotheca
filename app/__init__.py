6b2f096d175d5d376eb81e0b15515a6e13f816f8
pickles4evaaaa <king.gabriel@riseup.net>
2025-06-11 12:50:56 -0500

    update sql and docker path

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .models import db
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'SECRET_KEY_HERE'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/books.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
