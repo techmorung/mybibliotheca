a876058e73f47bd59464a6957f2198a7acba1649
techmorung <105538882+techmorung@users.noreply.github.com>
2025-06-25 11:59:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ from app import create_app
 app = create_app()
 if __name__ == '__main__':
     raise RuntimeError("❌ To run MyBibliotheca, use Gunicorn:\n\n    gunicorn -w NUMBER_OF_WORKERS -b 0.0.0.0:5054 run:app\n\nNote: Replace NUMBER_OF_WORKERS with the number of worker processes you want to run. A good starting point is 4 workers but most modern machines can handle more.")
+from fastapi import FastAPI
+
+app = FastAPI()
